178f8ca18e69a8c7cbd9c54c402df1172a38f5f3
TeeShihMin <37685824+TeeShihMin@users.noreply.github.com>
2018-05-21 23:18:47 +0800

    Update BikeType.php

--- BikeType.php
+++ BikeType.php
@@ -3,7 +3,7 @@ $_SESSION['logged']=false;
 session_start();
 
 ?>
-///
+/
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 kuantanbikerental//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-kuantanbikerental.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
